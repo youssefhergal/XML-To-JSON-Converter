220f4bd57c6892f96561133e6d1aa74f818ca0b7
youssefhergal <youssefher247@gmail.com>
2023-11-28 00:30:10 +0100

    Adding some Fetuares (Balise styling and autocompteing

--- src/InterfaceSwing.java
+++ src/InterfaceSwing.java
@@ -1,92 +1,112 @@
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rtextarea.RTextScrollPane;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.nio.file.Files;
 
 public class InterfaceSwing extends JFrame {
     private JPanel panelHeader;
-
-    private JTextArea textAreaLeft;
+    private JButton xmlToButton;
+    private JButton yamlToButton;  // Correction : Renommé de YamlToButton à yamlToButton
+    private JButton jsonToButton;  // Correction : Renommé de xmlToButton à jsonToButton
+    private RSyntaxTextArea textAreaLeft;
     private JButton centerButton;
     private JButton centerButton1;
     private JButton uploadButton;
-    private JTextArea textAreaRight;
+    private RSyntaxTextArea textAreaRight;
     private XmlJsonConverter xmlJsonConverter;
 
     public InterfaceSwing() {
         setTitle("Xml To Json Converter");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 500);
+        setSize(1100, 600);
         setLocationRelativeTo(null);
-        setResizable(false);
 
-        // Initialisation de la classe XmlJsonConverter
+        // Initialize the XmlJsonConverter class
         xmlJsonConverter = new XmlJsonConverter();
 
-        // Création des composants
+        // Create components
         panelHeader = new JPanel();
-        textAreaLeft = new JTextArea();
+        xmlToButton = new JButton("XML TO ");
+        yamlToButton = new JButton("YAML TO ");
+        jsonToButton = new JButton("JSON TO ");
+        textAreaLeft = new RSyntaxTextArea();
         centerButton = new JButton("XML TO JSON");
         centerButton1 = new JButton("XML TO YAML");
         uploadButton = new JButton("UPLOAD  XML");
-        textAreaRight = new JTextArea();
+        textAreaRight = new RSyntaxTextArea();
 
-        // Ajustement de la taille des composants
-        textAreaLeft.setPreferredSize(new Dimension(300, 300));
+        // Set syntax highlighting
+        textAreaLeft.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_XML);
+        textAreaRight.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JSON);
+
+        // Set component sizes
+        textAreaLeft.setPreferredSize(new Dimension(400, 300));
         centerButton.setPreferredSize(new Dimension(120, 40));
         centerButton1.setPreferredSize(new Dimension(120, 40));
         uploadButton.setPreferredSize(new Dimension(120, 40));
-        textAreaRight.setPreferredSize(new Dimension(300, 300));
+        textAreaRight.setPreferredSize(new Dimension(400, 300));
 
-        panelHeader.setBackground(new Color(0x458796));
-        panelHeader.setPreferredSize(new Dimension(getWidth(), 30));
+        panelHeader.setBackground(new Color(0x90B8C1));
+        panelHeader.setPreferredSize(new Dimension(getWidth(), 40));
 
+        RTextScrollPane scrollPaneLeft = new RTextScrollPane(textAreaLeft);
+        RTextScrollPane scrollPaneRight = new RTextScrollPane(textAreaRight);
+
+        // Set layout
+        setLayout(new BorderLayout());
         JPanel panelLeft = new JPanel();
         panelLeft.setLayout(new BorderLayout());
-        panelLeft.add(new JScrollPane(textAreaLeft, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED), BorderLayout.CENTER);
+        panelLeft.add(scrollPaneLeft, BorderLayout.CENTER);
 
         JPanel panelCenter = new JPanel();
-        panelCenter.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 30));
+        panelCenter.setSize(120, 600);
+        panelCenter.setLayout(new GridLayout(6, 1, 40, 40));
         panelCenter.add(centerButton);
         panelCenter.add(centerButton1);
         panelCenter.add(uploadButton);
 
         JPanel panelRight = new JPanel();
         panelRight.setLayout(new BorderLayout());
-        panelRight.add(new JScrollPane(textAreaRight, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED), BorderLayout.CENTER);
+        panelRight.add(scrollPaneRight, BorderLayout.CENTER);
 
-        setLayout(new BorderLayout());
+        // Add buttons to the header panel
+        panelHeader.add(xmlToButton);
+        panelHeader.add(yamlToButton);
+        panelHeader.add(jsonToButton);
 
         add(panelHeader, BorderLayout.NORTH);
         add(panelLeft, BorderLayout.WEST);
         add(panelCenter, BorderLayout.CENTER);
         add(panelRight, BorderLayout.EAST);
 
-        centerButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                onConvertButtonClicked();
-            }
-        });
-
-        uploadButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                onUploadButtonClicked();
-            }
-        });
+        xmlToButton.addActionListener(e -> onXmlToButtonClicked());
+        yamlToButton.addActionListener(e -> onYamlToButtonClicked());
+        jsonToButton.addActionListener(e -> onJsonToButtonClicked());
+        centerButton.addActionListener(e -> onConvertButtonClicked());
+        uploadButton.addActionListener(e -> onUploadButtonClicked());
 
         setVisible(true);
     }
 
-    private void onConvertButtonClicked() {
-        // Récupération du texte XML du premier JTextArea
-        String xmlInput = textAreaLeft.getText();
+    private void onYamlToButtonClicked() {
+        // TODO: Gérer la conversion YAML
+    }
 
-        // Appel de la méthode de conversion de la classe XmlJsonConverter
+    private void onXmlToButtonClicked() {
+        // TODO: Gérer la conversion XML
+    }
+
+    private void onJsonToButtonClicked() {
+        // TODO: Gérer la conversion JSON
+    }
+
+    private void onConvertButtonClicked() {
+        String xmlInput = textAreaLeft.getText();
         try {
             String jsonOutput = xmlJsonConverter.convertXmlToJson(xmlInput);
             textAreaRight.setText(jsonOutput);
@@ -115,6 +135,7 @@ public class InterfaceSwing extends JFrame {
         }
     }
 
-
-
+    public static void main(String[] args) {
+        new InterfaceSwing();
+    }
 }
